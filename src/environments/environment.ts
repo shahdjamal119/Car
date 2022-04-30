1ce42ab19dfa95b121187723fcaf5bde07e08a21
ABOODMASRI2 <92644553+ABOODMASRI2@users.noreply.github.com>
2022-05-01 01:45:14 +0300

    add authontication

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+firebase : {
+    apiKey: "AIzaSyB2Bmtrpi-8HFngr0Tw6tBP-w2X0zDflUY",
+    authDomain: "authonti.firebaseapp.com",
+    projectId: "authonti",
+    storageBucket: "authonti.appspot.com",
+    messagingSenderId: "888928132938",
+    appId: "1:888928132938:web:37e34870c024d797862bb3"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
