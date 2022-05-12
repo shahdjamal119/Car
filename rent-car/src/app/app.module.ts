1ac452414229234a2f554e6ac7c65939243659f2
abood <75145796+aboodghalayini@users.noreply.github.com>
2022-05-13 01:10:06 +0300

    creat component whith angular

---
+++ rent-car/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './rc/header/header.component';
+import { HomeComponent } from './rc/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
