176563b795cd5f2830695f038c2c72e5ab495afc
TamaraAwaisa <91056871+TamaraAwaisa@users.noreply.github.com>
2022-05-27 10:56:08 +0300

    Add files via upload

---
+++ scrollReveal/main.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.form-container form',{delay: 800, origin: 'left'})
