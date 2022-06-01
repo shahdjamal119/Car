89c96968991936b3b06606adaa749c528c42242a
mac <mac@macbook-pro-mac.mynet>
2022-06-01 15:06:29 +0300

    add & delete car [Amjad]

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Good } from 'src/app/interfaces/good.interface';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  cars: Good[] = [];
+  carObj: Good = {
+    id: '',
+    name: '',
+    price: 0,
+  };
+  id: string = '';
+  name: string = '';
+  price: number = 0;
+
+  constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
+    this.getAllCars();
   }
 
+  getAllCars() {
+    this.crudService.getAllCars().subscribe(res => {
+      this.cars = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data
+      })
+    })
+  }
+
+  resetForm() {
+    this.id = '';
+    this.name = '';
+    this.price = 0;
+    this.carObj.id = '';
+    this.carObj.name = '';
+    this.carObj.price = 0;
+  }
+
+  addCar() {
+    if(this.name == '' || this.price == 0) {
+      alert('Fill All Field');
+      return;
+    } 
+    this.carObj.id = '';
+    this.carObj.name = this.name;
+    this.carObj.price = this.price
+    this.crudService.addCar(this.carObj);
+    this.resetForm();
+  }
+
+
+  deleteCar(car: Good) {
+    
+    if(window.confirm('Are you sure you want to delete ' + car.name + ' ? ')) {
+      this.crudService.deleteCar(car)
+    }
+  }
 }
